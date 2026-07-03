debbbc599d12b477fadb6d50601099188a46a485
vieiraailton-dev <vieira.ailton@escola.pr.gov.br>
2026-07-03 12:45:12 -0300

    Add files via upload

---
+++ exercicio patrico/script.js
@@ -0,0 +1,19 @@
+function modoClaro() {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black"
+
+
+    document.getElementById("mensagem").innerText =
+    "Modo claro ativado!";
+}
+
+
+function modoEscuro() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white"
+
+
+    document.getElementById("mensagem").innerText =
+    "Modo escuro ativado!";
+
+}
